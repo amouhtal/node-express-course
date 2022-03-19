583a02ed754ca8fc2841c0d0be35f8d1bea32ca2
amouhtal <mouhtal.abderrahmane@gmail.com>
2022-03-19 17:25:45 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,17 +1,42 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 
 const mockUserData=[
 	{name:'Mark'},
 	{name:'Jill'}
 ]
+
 app.get('/users', function(req,res){
 	res.json({
 		success: true,
 		message: 'successfully got users. Nice!',
 		users: mockUserData
 	})
+})
+
+
+app.post('/login',function(req,res){
+	const username = req.body.username;
+	const password = req.body.password;
+
+	const mockUsername="billyTheKid";
+	const mockPassword="superSecret";
+
+	if (username===mockUsername && password===mockPassword){
+		 res.json({
+			  success: true,
+			  message: 'password and username match!',
+			  token: 'encrypted token goes here'
+		 })
+	} else {
+		 res.json({
+			  success: false,
+			  message: 'password and username do not match'
+		 })
+	}
 })
 
 app.listen(8000, function()
